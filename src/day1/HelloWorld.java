ce2dcd8ee7fd8c8ac9e5323dad3d8bd5a4137b04
ayfersarikoc <51866842+ayfersarikoc@users.noreply.github.com>
2019-06-16 00:27:28 +0100

    Update HelloWorld.java

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -1,8 +1,8 @@
-package day1;
+ppackage day1;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Eclipse!");
 		System.out.println("Hello git!");
 //		System.out.println("Hello World!");
 	}
